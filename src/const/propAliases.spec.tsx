946a8fef427ad40d75439ef66e89964887d0ca27
kettanaito <kettanaito@gmail.com>
2019-04-14 21:19:00 +0200

    Aligns prop alias interface

--- src/const/propAliases.spec.tsx
+++ src/const/propAliases.spec.tsx
@@ -55,12 +55,12 @@ describe('Prop aliases', () => {
       const domElement = getByTestId(container, 'composition')
 
       /* Assertion */
-      const { props: cssProps, transformValue } = propAliases[propAliasName]
+      const { output, transformValue } = propAliases[propAliasName]
       const expectedValue = transformValue
         ? transformValue(propValue)
         : propValue
 
-      cssProps.forEach((cssPropName) => {
+      output.forEach((cssPropName) => {
         expect(domElement).toHaveStyle(`${cssPropName}:${expectedValue}`)
       })
     })
